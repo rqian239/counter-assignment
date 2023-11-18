1b48f8a61c616021916667e06f67bced277b2ace
Richard Qian <richardqiancs@gmail.com>
2023-11-17 23:04:23 -0500

    Render the counter component

--- src/tests/Counter.test.js
+++ src/tests/Counter.test.js
@@ -1,8 +1,12 @@
 // import necessary react testing library helpers here
 // import the Counter component here
 
+import { render } from "@testing-library/react";
+import Counter from "../components/Counter";
+
 beforeEach(() => {
-  // Render the Counter component here
+  // Render the Counter component
+  render(<Counter />);
 })
 
 test('renders counter message', () => {
